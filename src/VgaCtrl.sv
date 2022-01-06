747f3b594bdec5e84b7f715024750c2c71e5295c
Akitaki <robinhuang123@gmail.com>
2022-01-06 10:18:43 +0800

    Coding style tweaks

--- src/VgaCtrl.sv
+++ src/VgaCtrl.sv
@@ -12,20 +12,14 @@ module VgaCtrl(
     logic hsync_i,vsync_i;
     logic hsync_default, vsync_default;
     logic [9:0] HD, HF, HS, HT, VD, VF, VS, VT;
-    // unused
-    // logic [9:0] HB, VB;
 
     assign HD = 640;
     assign HF = 16;
     assign HS = 96;
-    // unused
-    // assign HB = 48;
     assign HT = 800; 
     assign VD = 480;
     assign VF = 10;
     assign VS = 2;
-    // unused
-    // assign VB = 33;
     assign VT = 525;
     assign hsync_default = 1'b1;
     assign vsync_default = 1'b1;
@@ -34,10 +28,11 @@ module VgaCtrl(
         if (reset) begin
             pixel_cnt <= 0;
         end else begin
-            if (pixel_cnt < (HT - 1))
+            if (pixel_cnt < (HT - 1)) begin
                 pixel_cnt <= pixel_cnt + 1;
-            else
+            end else begin
                 pixel_cnt <= 0;
+            end
         end
     end
 
@@ -46,10 +41,11 @@ module VgaCtrl(
             hsync_i <= hsync_default;
         end else begin
             if ((pixel_cnt >= (HD + HF - 1))
-            &&(pixel_cnt < (HD + HF + HS - 1)))
+                &&(pixel_cnt < (HD + HF + HS - 1))) begin
                 hsync_i <= ~hsync_default;
-            else
+            end else begin
                 hsync_i <= hsync_default; 
+            end
         end
     end
     
@@ -58,10 +54,11 @@ module VgaCtrl(
             line_cnt <= 0;
         end else begin
             if (pixel_cnt == (HT -1)) begin
-                if(line_cnt < (VT - 1))
+                if (line_cnt < (VT - 1)) begin
                     line_cnt <= line_cnt + 1;
-                else
+                end else begin
                     line_cnt <= 0;
+                end
             end
         end
     end
@@ -71,10 +68,11 @@ module VgaCtrl(
             vsync_i <= vsync_default; 
         end else begin
             if ((line_cnt >= (VD + VF - 1))
-                &&(line_cnt < (VD + VF + VS - 1)))
+                &&(line_cnt < (VD + VF + VS - 1))) begin
                 vsync_i <= ~vsync_default; 
-            else
+            end else begin
                 vsync_i <= vsync_default; 
+            end
         end
     end
                     
@@ -82,6 +80,6 @@ module VgaCtrl(
     assign vsync = vsync_i;
     assign valid = ((pixel_cnt < HD) && (line_cnt < VD));
     
-    assign h_cnt = (pixel_cnt < HD) ? pixel_cnt:10'd0;
-    assign v_cnt = (line_cnt < VD) ? line_cnt:10'd0;
+    assign h_cnt = (pixel_cnt < HD) ? pixel_cnt : 10'd0;
+    assign v_cnt = (line_cnt < VD) ? line_cnt : 10'd0;
 endmodule
